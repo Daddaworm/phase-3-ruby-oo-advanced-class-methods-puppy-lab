d736d60bf63529d289bf3283dac60c91689eedb8
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-07 13:36:53 -0400

    not completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+require 'pry'
+
+
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog
+        end
+        # binding.pry
+    end
+
+
+end
